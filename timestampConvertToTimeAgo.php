494ff28d2e9ef193cfafe5feb26cb34a6b344fd7
Christopher Waldau <cwal@users.noreply.github.com>
2017-02-21 12:48:23 -0500

    formatting
    2 spaces indent, add spaces after if/elseif/else/foreach etc and always use curlay braces (even for 1 line if's)

--- timestampConvertToTimeAgo.php
+++ timestampConvertToTimeAgo.php
@@ -9,114 +9,100 @@
 	 
 	function timestampConvertToTimeAgo($timestamp, $accuracy = 6, $now = Null)
 	{
-		if( LANG == 'en')
-		{
+		if (LANG == 'en') {
 			$periods = array("s" => "second", "i" => "minute", "h" => "hour", "d" => "day", "m" => "month", "y" => "year");
-		}
-		else
-		{
+		} else {
 			$periods = array("s" => "seconde", "i" => "minute", "h" => "heure", "d" => "jour", "m" => "mois", "y" => "année");
 		}
 			
-		if ($now === Null) 
-		{
+		if ($now === Null) {
 			$now = date('Y-m-d H:i:s');
 		}
 		
 		// get an array with the number of years/months/days/hours/minutes/seconds difference between dates
-		$d1 = new DateTime( $timestamp );
-		$d2 = new DateTime( $now );
+		$d1 = new DateTime($timestamp);
+		$d2 = new DateTime($now);
 		$diff = $d2->diff($d1);
 		
 		$result = '';
 		$i = 0;
-		foreach($diff as $k => $v)
-		{
+		foreach ($diff as $k => $v) {
 			// check if the key is in our periods array ($diff also contains other information like if its a weekday)
 			// also check if the value is not 0 to skip through differences where it has not been a year/month/etc yet
-			if( array_key_exists($k, $periods) && $v )
-			{
+			if (array_key_exists($k, $periods) && $v) {
 				// less than an hour ago		
-				if( ( ($k == 'i' || $k == 's') && $result == '')  && $accuracy == 1 )
-				{
-					if(LANG == 'en')
+				if ((($k == 'i' || $k == 's') && $result == '') && $accuracy == 1) {
+					if (LANG == 'en') {
 						$result = 'moments';
-					else
+          } else {
 						$result = 'quelques instants';
+          }
 				}
 				
 				// if accuracy is 1 and its been an hour
-				elseif($v == 1 && $periods[$k] == 'hour' && $accuracy == 1)
-				{
+				elseif ($v == 1 && $periods[$k] == 'hour' && $accuracy == 1) {
 					$result .= 'an ' . $periods[$k];
 				}
-				elseif($v == 1 && $periods[$k] == 'heure' && $accuracy == 1)
-				{
+				elseif ($v == 1 && $periods[$k] == 'heure' && $accuracy == 1) {
 					$result .= 'une ' . $periods[$k];
 				}
 				
 				// french 1 year is 'ans' instead of 'annee'
-				elseif($v == 1 && $periods[$k] == 'année')
-				{
+				elseif ($v == 1 && $periods[$k] == 'année') {
 					$result .= 'un ans';
 				} 
 				
 				// make plural if larger than 1
-				elseif($v > 1 && $periods[$k] != 'mois') // dont make 'mois' plural
-				{
+				elseif ($v > 1 && $periods[$k] != 'mois') { // dont make 'mois' plural
 					$result .= $v . ' ' . $periods[$k] . 's';
 				}
 				// otherwise its 1 unit so not plural
-				else
-				{
+				else {
 					$result .= $v . ' ' . $periods[$k];
 				}
 				
 				// add a space between periods 
-				if($accuracy > 1)
+				if ($accuracy > 1) {
 					$result .= ' ';
+        }
 				
 				$i++;
 					
 			}
 			
 			//break the loop if desired accuracy is reached
-			if($i == $accuracy)
-			{
+			if ($i == $accuracy) {
 				break;
 			}
 		}
 		
 		// datetime stamps are exactly the same so $result is empty.
-		if($d1 == $d2)
-		{
-			if(LANG == 'en')
+		if ($d1 == $d2) {
+			if (LANG == 'en') {
 				$result = 'just now';
-			else 
+      } else {
 				$result = 'juste maintenant';
+      }
 		}
 		// if we have a string in result append the ago/future
-		elseif($result)
-		{
+		elseif ($result) {
 			// if invert is 0, then timestamp was in the future (timestamp > now)
-			if( $diff->invert == 0 )
-			{
-				if(LANG == 'en')
+			if ($diff->invert == 0) {
+				if (LANG == 'en') {
 					$result = $result . ' in the future';
-				else 
+        } else {
 					$result = $result . ' dans le future';
-			}
-			else
-			{
-				if(LANG == 'en')
+        }
+			} else {
+				if (LANG == 'en') {
 					$result = $result . ' ago';
-				else 
+        } else {
 					$result = 'il y a ' . $result;
+        }
 			}
-		}
-		else
+		} else {
 			$result = 'undefined'; // fallback if everything fails
-			
+    }
 		
 		return $result;
 	}
